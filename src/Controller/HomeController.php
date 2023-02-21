016d46b70210a46abe9561f0eba06b09abaab239
mouzzine2022 <mohammed.ouzzine2022@campus-eni.fr>
2023-02-21 10:54:33 +0100

    base html.twig

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class HomeController extends AbstractController
+{
+    #[Route(path: '', name: 'home', methods: ['GET'])]
+
+    public function home(): \Symfony\Component\HttpFoundation\Response
+    {
+        return $this->render('home/home.html.twig');
+    }
+
+    #[Route(path: 'about', name: 'about', methods: ['GET'])]
+    public function about(): \Symfony\Component\HttpFoundation\Response
+    {
+        return $this->render('home/about.html.twig');
+    }
+
+    #[Route(path: 'legal-stuff', name: 'legal_stuff', methods: ['GET'])]
+    public function legalStuff(): Response
+    {
+        return $this->render('home/legal_stuff.html.twig');
+    }
+
+
+}
