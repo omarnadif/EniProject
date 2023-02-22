6aaea3641e9f4bacd6cb01f1a8ce4d5a68a91b0e
onadif2022 <omar.nadif2022@campus-eni.fr>
2023-02-22 16:56:51 +0100

    Omar

--- src/Controller/ExcursionController.php
+++ src/Controller/ExcursionController.php
@@ -2,8 +2,16 @@
 
 namespace App\Controller;
 
+use App\Entity\Sortie;
+use App\Form\CreerSortieFormType;
+use App\Security\UserAuthenticator;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
 
 #[Route(path: 'excursion/')]
 class ExcursionController extends AbstractController
@@ -14,11 +22,6 @@ class ExcursionController extends AbstractController
         return $this->render('excursions/excursion.html.twig');
     }
 
-    #[Route(path: 'edite', name: 'editeExcursion', methods: ['GET'])]
-    public function editeExcursion(): \Symfony\Component\HttpFoundation\Response
-    {
-        return $this->render('excursions/EditeExcursion.html.twig');
-    }
 
     #[Route(path: 'update', name: 'updateExcursion', methods: ['GET'])]
     public function updateExcursion(): \Symfony\Component\HttpFoundation\Response
@@ -26,4 +29,34 @@ class ExcursionController extends AbstractController
         return $this->render('excursions/updateExcursion.html.twig');
     }
 
+    #[Route('edite', name: 'editeExcursion', methods: ['GET', 'POST'])]
+    public function excursionForm(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager, UserAuthenticator $authenticator, UserAuthenticatorInterface $userAuthenticator): Response
+    {
+
+        // Création
+        $sortie = new Sortie();
+
+
+        //Création du formulaire
+        $form = $this->createForm(CreerSortieFormType::class, $sortie);
+        $form->handleRequest($request);
+
+
+        //Vérification du formulaire
+        if ($form->isSubmitted() && $form->isValid()) {
+
+
+            //Insertion du Participant en BDD (Base de donnée)
+            $entityManager->persist($sortie);
+            $entityManager->flush();
+
+            //Connexion automatique du Participant
+//            return $userAuthenticator->authenticateUser($user, $authenticator, $request);
+
+        }
+
+        return $this->render('excursions/EditeExcursion.html.twig', [
+            'excursionForm' => $form->createView(),
+        ]);
+    }
 }
