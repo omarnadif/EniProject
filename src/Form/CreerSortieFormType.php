6cacd0fcb2a2e7d8987df8095cbd21b81e1ebed2
MOHAMMED-OUZZINE <mohammed.ouzzine2022@campus-eni.fr>
2023-03-02 23:13:04 +0100

    controle input creer sortie

--- src/Form/CreerSortieFormType.php
+++ src/Form/CreerSortieFormType.php
@@ -29,14 +29,35 @@ class CreerSortieFormType extends AbstractType
             'trim' => true,
             'label' => 'Nom: ',
             'required' => true,
-            'attr' => ['class' => 'form-control']
+            'constraints' => [
+                new Length([
+                    'min' => 2,
+                    'max' => 255,
+                    'minMessage' => 'Le nom doit contenir au moins {{ limit }} caractères.',
+                    'maxMessage' => 'Le nom ne peut pas contenir plus de {{ limit }} caractères.'
+                ]),
+                new Regex([
+                    'pattern' => '/^[a-zA-Z0-9 _-]+$/',
+                    'message' => 'Le nom ne peut contenir que des lettres, des chiffres, des espaces, des tirets et des underscores.'
+                ])
+            ]
         ]);
 
         $builder->add('dateHeureDebut', DateTimeType::class, [
             'widget'=>'single_text',
             'trim' => true,
-           'required' => true,
+            'required' => true,
             'attr' => ['class' => 'form-control'],
+            'constraints' => [
+                new GreaterThanOrEqual([
+                    'value' => 'today',
+                    'message' => 'La date et l\'heure de début doivent être postérieures ou égales à la date d\'aujourd\'hui.'
+                ]),
+                new LessThanOrEqual([
+                    'propertyPath' => 'parent.all[dateLimiteInscription].data',
+                    'message' => 'La date et l\'heure de début ne peuvent pas être postérieures à la date limite d\'inscription.'
+                ]),
+            ]
         ]);
 
         $builder->add('dateLimiteInscription', DateTimeType::class, [
@@ -44,18 +65,44 @@ class CreerSortieFormType extends AbstractType
             'trim' => true,
             'required' => true,
             'attr' => ['class' => 'form-control'],
+            'constraints' => [
+                new GreaterThanOrEqual([
+                    'value' => 'today',
+                    'message' => 'La date limite d\'inscription doit être postérieure ou égale à la date d\'aujourd\'hui.'
+                ]),
+                new LessThanOrEqual([
+                    'propertyPath' => 'parent.all[dateHeureDebut].data',
+                    'message' => 'La date limite d\'inscription ne peut pas être postérieure à la date et l\'heure de début de la sortie.'
+                ]),
+            ]
         ]);
 
         $builder->add('duree', IntegerType::class, [
             'trim' => true,
             'required' => true,
-            'attr' => ['class' => 'form-control']
+            'attr' => ['class' => 'form-control'],
+            'constraints' => [
+                new NotBlank([
+                    'message' => 'La durée ne doit pas être vide.',
+                ]),
+                new NotNull([
+                    'message' => 'La durée ne doit pas être nulle.',
+                ]),
+                new Positive([
+                    'message' => 'La durée doit être positive.',
+                ]),
+            ],
         ]);
 
         $builder->add('nbInscriptionsMax', IntegerType::class, [
             'trim' => true,
             'required' => false,
-            'attr' => ['class' => 'form-control']
+            'attr' => ['class' => 'form-control'],
+            'constraints' => [
+                new PositiveOrZero([
+                    'message' => 'Le nombre maximum d\'inscriptions doit être un nombre positif ou nul.'
+                ])
+            ]
         ]);
 
         $builder->add('sortieUploadPicture', FileType::class, [
